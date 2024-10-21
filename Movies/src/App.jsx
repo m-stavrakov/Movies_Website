64e2d9f2a43725cc9fbcd34cf785793960e4c4f5
m-stavrakov <131870370+m-stavrakov@users.noreply.github.com>
2024-10-21 13:47:31 +0100

    API fetch, css and page design

--- Movies/src/App.jsx
+++ Movies/src/App.jsx
@@ -1,34 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
+
+// e64a1b71
+
+const API_URL = 'http://www.omdbapi.com?apikey=e64a1b71';
+
+const movie1 = {
+  Poster: "https://m.media-amazon.com/images/M/MV5BMjA2NjU5MTg5OF5BMl5BanBnXkFtZTgwOTkyMzQxMDE@._V1_SX300.jpg",
+  Title: "Scream",
+  Type: "movie",
+  Year: "1996",
+  imdbID: "tt0117571"}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const searchMovies = async(title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+  
+  // fetching movies
+  useEffect(() => {
+    searchMovies('Scream');
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <h1>MovieWorld</h1>
+
+      <div className="search">
+        <input 
+          type="text"
+          placeholder='Find a movie'
+          value="Superman"
+          onChange={() => {
+
+          }} />
+        <img 
+          src={SearchIcon} 
+          alt="search"
+          onClick={() => {
+
+          }} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="container">
+          <div className="movie">
+            <div>
+              <p>{movie1.Year}</p>
+            </div>
+
+            <div>
+              <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title} />
+            </div>
+
+            <div>
+              <span>{movie1.Type}</span>
+              <h3>{movie1.Title}</h3>
+            </div>
+          </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
